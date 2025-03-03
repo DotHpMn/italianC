75db5f8640c4b7dc8f824df06edbc1d1954cf82b
DotHpMn <95506571+DotHpMn@users.noreply.github.com>
2025-03-03 13:49:52 +0100

    Create italianC.h

---
+++ italianC.h
@@ -0,0 +1,83 @@
+// to add translations to this file, just add the library, then add the keywords or function names with their translations ad: #define translation keyword/functionname
+
+#include <stdio.h>
+
+// c keywords
+
+#define auto automatico
+#define rompi break
+#define caso case
+#define carattere char
+#define costante const
+#define continua continue
+#define prefedinito default
+#define fai do
+#define doppio double
+#define senno else
+#define enumeratore enum
+#define esterno extern
+#define virgolafluttuante float
+#define per for
+#define vaia goto
+#define se if
+#define intero int
+#define lungo long
+#define registro register
+#define ritorna return
+#define corto short
+#define segno signed
+#define dimensionedi sizeof
+#define statico static
+#define struttura struct
+#define cambia switch
+#define definizioneditipo typedef
+#define unione union
+#define senzasegno unsigned
+#define vuoto void
+#define volatile volatile
+#define finche while
+
+// functions in stdio
+
+#define fchiudi fclose
+#define ffinedelfile feof
+#define ferrore ferror
+#define fprendic fgetc
+#define fprendis fgets
+#define fapri fopen
+#define fstampaf fprintf
+#define fmettic fputc
+#define fmettis fputs
+#define fleggi fread
+#define fscannerizzzaf fscanf
+#define fcerca fseek
+#define fracconta ftell
+#define fscrivi fwrite
+#define prendic getc
+#define prendicarattere getchar
+#define stampaf printf
+#define mettic putc
+#define metticarattere putchar
+#define mettis puts
+#define rimuovi remove
+#define rinomina rename
+#define riavvolgere rewind
+#define scannerizzaf scanf
+#define snstampaf snprintf
+#define sstampa sprint
+
+// secondary symbols
+
+#define e ,
+#define piu +
+#define meno -
+#define diviso /
+#define moltiplica *
+#define uguale =
+#define ugualeuguale ==
+#define minoreuguale <=
+#define maggioreuguale >=
+#define severo ?
+#define sefalso :
+#define apri {
+#define chiudi }
